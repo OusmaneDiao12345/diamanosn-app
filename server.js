b53cd003f7eb84cc125f737a379dbf428528c9d0
OusmaneDiao12345 <159960934+OusmaneDiao12345@users.noreply.github.com>
2026-02-22 02:44:11 +0000

    oui

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const axios = require('axios');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
 const PORT = process.env.PORT || 3000;
 const DB_PATH = path.join(__dirname, 'diamano.db');
 
